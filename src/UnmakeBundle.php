1cbfb11ee39f07a331f181f8a2e4303f7db12871
psalamontech <50994299+psalamontech@users.noreply.github.com>
2024-01-31 15:12:45 +0100

    UnmakeBundle

---
+++ src/UnmakeBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Hexis\UnmakeBundle;
+
+use Hexis\AccessControlProfilerBundle\DependencyInjection\Compiler\CustomPass;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UnmakeBundle extends Bundle
+{
+
+}
